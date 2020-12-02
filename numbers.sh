7c81937e890d2e81509d5bb8f6009f50dfadcb7a
Drew Bozarth <dbozarth@chapman.edu>
2020-12-02 00:14:36 -0800

    Fixes README and adds file

---
+++ numbers.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# numbers.sh
+# Drew Bozarth
+echo "Please enter a positive integer:"
+read -r X
+N=1
+while [ "$N" -le "$X" ]
+do
+	if [ $((N%2)) -eq 0 ]
+		then
+			echo "$N is Even"
+	else
+		echo "$N is Odd"
+	fi
+	N=$[$N+1]
+done	
